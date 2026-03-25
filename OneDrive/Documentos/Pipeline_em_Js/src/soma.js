bd2faa31ba007901d4843b8605383907c56d6a3f
Walisom Lopes Souza <walisom.souza@fatec.sp.gov.br>
2026-03-25 09:06:48 -0300

    ci: adiciona integracao com sonarcloud

--- OneDrive/Documentos/Pipeline_em_Js/src/soma.js
+++ OneDrive/Documentos/Pipeline_em_Js/src/soma.js
@@ -1,5 +1,20 @@
 function soma(a, b) {
+ let resultado = a + b;
+ let x = 10; 
+ return resultado;
+}
+
+function soma(a, b) {
  return a + b;
 }
+function soma2(a, b) {
+ return a + b;
+}
+
+function soma(a, b) {
+eval("console.log('teste')");
+return a + b;
+}
+
 module.exports = soma;
 
